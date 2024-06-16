a47202618601586cf3a0dc7bac2248e4d9dd9ee0
PainAbi <168978496+painabi@users.noreply.github.com>
2024-06-16 17:40:48 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+lua54 'yes'
+fx_version 'cerulean'
+game 'gta5'
+
+author '@painabixyz'
+description 'FiveM Anti RageBot Script for Cheats Like Nexus...'
+version '1.0.0'
+
+client_scripts {
+    "client.lua",
+    "config.lua"
+}
